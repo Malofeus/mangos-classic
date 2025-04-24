1b4dda66cdf274a38c86fbbb916a6e05199227ca
Malofeus <jmartin9817@gmail.com>
2025-04-24 10:32:56 -0500

    Fixed define unix locations

--- src/shared/SystemConfig.h
+++ src/shared/SystemConfig.h
@@ -100,14 +100,12 @@
  
  #ifdef ENABLE_PLAYERBOTS
     #define _D_PLAYERBOTS_CONFIG SYSCONFDIR"aiplayerbot.conf"
-    //#define _PLAYERBOTS_CONFIG MaNGOS::Singleton<std::string>::Instance()
-    #define _PLAYERBOTS_CONFIG ""
+    #define _PLAYERBOTS_CONFIG MaNGOS::Singleton<std::string>::Instance()
  #endif
  
  #ifdef ENABLE_SOLOCRAFT
      #define _D_SOLOCRAFT_CONFIG SYSCONFDIR"SoloCraft.conf"
-     //#define _SOLOCRAFT_CONFIG MaNGOS::Singleton<std::string>::Instance()
-     #define _SOLOCRAFT_CONFIG ""
+     #define _SOLOCRAFT_CONFIG MaNGOS::Singleton<std::string>::Instance()
  #endif
  
  #define _FULLVERSION(REVD,REVH) _PACKAGENAME "/" _VERSION(REVD,REVH) " for " _ENDIAN_PLATFORM
